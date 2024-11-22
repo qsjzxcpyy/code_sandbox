1361530367d75a3271490ba6946f401790585c45
qi shijun <2100630861@qq.com>
2024-11-22 10:34:21 +0800

    解决并发问题

--- src/main/java/com/qsj/acojcodesandbox/controller/MainController.java
+++ src/main/java/com/qsj/acojcodesandbox/controller/MainController.java
@@ -4,6 +4,8 @@ import com.qsj.acojcodesandbox.JavaNativeCodeSandbox;
 import com.qsj.acojcodesandbox.model.ExecuteCodeRequest;
 import com.qsj.acojcodesandbox.model.ExecuteCodeResponse;
 import javassist.tools.web.BadHttpRequest;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,23 +24,39 @@ public class MainController {
 
     @Resource
     JavaNativeCodeSandbox javaNativeCodeSandbox;
-@GetMapping("/health")
-    public String healthCheck(){
-      return "ok";
+    @Resource
+    private RedissonClient redissonClient;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
     }
 
     @PostMapping("/executeCode")
     ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request
-            , HttpServletResponse response){
-        String secretKey= request.getHeader(AUTH_REQUEST_HEADER);
-        if(!AUTH_REQUEST_SECRET.equals(secretKey)){
+            , HttpServletResponse response) throws Exception {
+        String secretKey = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(secretKey)) {
             response.setStatus(403);
             return null;
         }
-        if(executeCodeRequest == null){
-        throw new RuntimeException("参数为空");
-    }
-        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("参数为空");
+        }
+        RLock rLock = redissonClient.getLock(String.valueOf("sss".hashCode()));
+        ExecuteCodeResponse executeCodeResponse = null;
+        rLock.lock();
+        try {
+
+         executeCodeResponse = javaNativeCodeSandbox.executeCode(executeCodeRequest);
+        } catch (Throwable e) {
+            throw new Exception(e);
+        } finally {
+            rLock.unlock();
+        }
+
+
+        return executeCodeResponse;
 
     }
 
